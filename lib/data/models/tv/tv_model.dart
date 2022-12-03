f9f493001052cab96cf564570cd98030c5d40b27
TedoHarisC <tedoharischandra29@gmail.com>
2022-12-03 13:22:45 +0700

    Initial commit

---
+++ lib/data/models/tv/tv_model.dart
@@ -0,0 +1,94 @@
+import 'package:ditonton/domain/entities/tv/tv.dart';
+import 'package:equatable/equatable.dart';
+
+class TVModel extends Equatable {
+  TVModel({
+    required this.backdropPath,
+    required this.genreIds,
+    required this.id,
+    required this.name,
+    required this.originCountry,
+    required this.originalLanguage,
+    required this.originalName,
+    required this.overview,
+    required this.popularity,
+    required this.posterPath,
+    required this.voteAverage,
+    required this.voteCount,
+  });
+
+  final String? backdropPath;
+  final List<int> genreIds;
+  final int id;
+  final String name;
+  List<String> originCountry;
+  String originalLanguage;
+  String originalName;
+  String overview;
+  double popularity;
+  String? posterPath;
+  double voteAverage;
+  int voteCount;
+
+  factory TVModel.fromJson(Map<String, dynamic> json) => TVModel(
+        backdropPath: json["backdrop_path"],
+        genreIds: List<int>.from(json["genre_ids"].map((x) => x)),
+        id: json["id"],
+        name: json["name"],
+        originCountry: List<String>.from(json["origin_country"].map((x) => x)),
+        originalLanguage: json["original_language"],
+        originalName: json["original_name"],
+        overview: json["overview"],
+        popularity: json["popularity"].toDouble(),
+        posterPath: json["poster_path"],
+        voteAverage: json["vote_average"].toDouble(),
+        voteCount: json["vote_count"],
+      );
+
+  Map<String, dynamic> toJson() => {
+        "backdrop_path": backdropPath,
+        "genre_ids": List<dynamic>.from(genreIds.map((x) => x)),
+        "id": id,
+        "name": name,
+        "origin_country": List<dynamic>.from(originCountry.map((x) => x)),
+        "original_language": originalLanguage,
+        "original_name": originalName,
+        "overview": overview,
+        "popularity": popularity,
+        "poster_path": posterPath,
+        "vote_average": voteAverage,
+        "vote_count": voteCount,
+      };
+
+  TV toEntity() {
+    return TV(
+        backdropPath: backdropPath,
+        genreIds: genreIds,
+        id: id,
+        name: name,
+        originCountry: originCountry,
+        originalLanguage: originalLanguage,
+        originalName: originalName,
+        overview: overview,
+        popularity: popularity,
+        posterPath: posterPath,
+        voteAverage: voteAverage,
+        voteCount: voteCount);
+  }
+
+  @override
+  List<Object?> get props => [
+        backdropPath,
+        genreIds,
+        id,
+        name,
+        originalLanguage,
+        originalName,
+        originCountry,
+        overview,
+        popularity,
+        posterPath,
+        voteAverage,
+        voteCount,
+      ];
+}
